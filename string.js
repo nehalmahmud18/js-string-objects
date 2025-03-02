72bf3258103227c2d9804430ec49d444ebdb008a
Nehal Mahmud <mahmudnehal83@gmail.com>
2025-03-02 06:14:47 +0600

    first commit

---
+++ string.js
@@ -0,0 +1,13 @@
+const country = 'bangladesh';
+const division = "noakhali";
+const district = `Noakhali`;
+const thana = new String('maijdi');
+
+// console.log(typeof thana)
+
+console.log(country.length);
+console.log(country[2]);
+country[2]= 'r';
+console.log(country);
+// string is immutable --- not changable.
+// array is immutable
