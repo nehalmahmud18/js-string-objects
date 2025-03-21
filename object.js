fa27b4f0eb49dfeb8067ad0829c98cc70e0d5b39
Nehal Mahmud <mahmudnehal83@gmail.com>
2025-03-21 10:59:53 +0600

    learned basic object

---
+++ object.js
@@ -0,0 +1,21 @@
+// object 
+//  non- primitive data 
+const bottle = {
+    brand: 'mum',
+    price: 45, 
+    color: 'white',
+    isClean: true
+}
+
+const subject ={
+    name: 'biology',
+    teacher: 'rasheda mam',
+    examDate: '23 feb',
+    chapters: ['1st', '2nd', '3rd'],
+    exams: {
+        name: 'final exam',
+        marks: 100
+    }
+
+
+}
